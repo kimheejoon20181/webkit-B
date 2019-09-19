18999fac8f65169c108afc0281b4b6d498da36e6
dit08 <dit@imac-3.local>
2019-09-19 14:55:02 +0900

    2019 09 19

--- webkit B/ViewController.swift
+++ webkit B/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UISearchBarDelegate,WKNavigationDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var searchBer: UISearchBar!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchBer.delegate = self
+        webView.navigationDelegate = self
+        
+        let request = URLRequest(url: URL(string: "https://www.naver.com")!)
+        webView.load(request)
+        
+        backButton.isEnabled = self.webView.canGoBack
+        forwardButton.isEnabled = self.webView.canGoForward
+        
+        indicator.isHidden = true
     }
 
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //print("Search Bar Click")
+        let input = searchBar.text
+        print(input!)
+        let request = URLRequest(url: URL(string: input!)!)
+        webView.load(request)
+    }
 
+    @IBAction func backAction(_ sender: Any) {
+        if self.webView.canGoBack{
+            self.webView.goBack()
+        }
+    }
+    
+    @IBAction func reloadAction(_ sender: Any) {
+        self.webView.reload()
+    }
+    
+    @IBAction func forwardAction(_ sender: Any) {
+        if self.webView.canGoForward{
+            self.webView.goForward()
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        indicator.isHidden = false
+        indicator.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        backButton.isEnabled = self.webView.canGoBack
+        forwardButton.isEnabled = self.webView.canGoForward
+        
+        indicator.stopAnimating()
+        indicator.isHidden = true
+    }
+    
 }
 
